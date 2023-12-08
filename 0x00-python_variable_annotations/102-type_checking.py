cd22a2060fe5abe097c60ddc560201eca4976370
0marSalah <omar.salah1597@gmail.com>
2023-12-08 22:33:20 +0200

    fix mypy errors

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 """ Complex types - mixed list """
 
-from typing import Tuple, List
+from typing import List
 
 
-def zoom_array(lst: Tuple, factor: float = 2) -> List:
+def zoom_array(lst: List, factor: float = 2.0) -> List:
     """ Return a tuple """
     zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
     ]
     return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
