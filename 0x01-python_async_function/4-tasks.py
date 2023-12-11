2c8f9aa26fb684afc758c73a568b84ca935e069f
0marSalah <omar.salah1597@gmail.com>
2023-12-11 19:36:30 +0200

    create task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,4 +11,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     tasks = [task_wait_random(max_delay) for i in range(n)]
     for task in asyncio.as_completed(tasks):
         delays.append(await task)
-    return delays
+    return sorted(delays)
