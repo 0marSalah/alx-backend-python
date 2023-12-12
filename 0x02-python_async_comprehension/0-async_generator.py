de4b76173696fbb0c6e52d05002df33deff0ae7e
0marSalah <omar.salah1597@gmail.com>
2023-12-12 22:16:39 +0200

    async first time

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,8 +8,6 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """ The coroutine will loop 10 times """
-    res = []
     for _ in range(10):
         await asyncio.sleep(1)
-        res.append(random.uniform(0, 10))
-    return res
+        yield (random.uniform(0, 10))
